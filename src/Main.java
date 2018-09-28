ef8a52121d078ce52fe0f5c7ec22cc6b6626db3a
Flint <26605@ma-web.nl>
2018-09-28 15:10:16 +0200

    test

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+
+public class Main{
+
+    public static void main(String[] args) {
+
+        boolean aanUit = true;
+
+
+        String stukjeTekst = "Wat Leuk!";
+
+        String andereTekst = "Echt heh :D";
+
+        String samenTekst = stukjeTekst + andereTekst;
+
+        int getalA = 123;
+
+        float getalB = 5.5f;
+
+        double getalC = 10.3d;
+
+        System.out.println (aanUit);
+
+        System.out.println(samenTekst);
+
+        System.out.println(getalA + getalB + getalC);
+
+    }
+}
